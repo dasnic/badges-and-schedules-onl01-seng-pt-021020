ac5e8446b130a47cf4d564fb4399c19ab6c7b166
Lydell Gaines <dasnic70@gmail.com>
2020-02-19 20:33:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(str)
+  "Hello, my name is #{str}."
+end
+
+def batch_badge_creator(arr)
+  new_arr = []
+  arr.each do |name|
+  "Hello, my name is #{name}."  
+end
+  new_arr
+end
+
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+  
+end
