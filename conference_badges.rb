92c705a7953dd9dad7abd28e1ebc5d2913116a69
Lydell Gaines <dasnic70@gmail.com>
2020-02-19 20:49:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,8 @@ end
 def assign_rooms(arr_of_names)
   counter = 1
   arr_of_names.map do |attendee|
-    puts Hello, #{attendee}! You'll be assigned to room #{}!
+    puts Hello, #{attendee}! You'll be assigned to room #{counter}!
+    counter += 1
 end
 
 def printer(attendees)
