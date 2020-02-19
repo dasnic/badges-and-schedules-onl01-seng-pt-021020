d153a6d1be4c088faef948d29ab902d7e6c35c6a
Lydell Gaines <dasnic70@gmail.com>
2020-02-19 21:14:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,5 @@ end
 
 def printer(arr_of_attendees)
   batch_badge_creator(arr_of_attendees).each {|phrase| puts phrase}
-  assign_rooms(arr_of_attendees)
+  assign_rooms(arr_of_attendees).each {|phrase| puts phrase}
 end
